1557fffb90f982843c66138e89f90c866ced1d61
JustinBeaudry <justin.beaudry+github@creativelive.com>
2015-05-17 01:18:25 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,35 @@
+Molecule
+========
+
+A CLI tool for backing up and restoring Atom Packages.
+
+## Options
+
+```
+-b backup atom packages to ~/.atom/manifest.json, by default backup will ignore disabledPackages
+[-d] forces molecule to backup disabledPackages
+
+-r restores packages from ~/.atom/manifest.json
+[-d] silence install output from apm
+
+-h output help
+--help
+```
+
+## Support
+
+* Node v0.12.2
+* MAC OS X 10.10
+
+## TODO
+
+* install pacakge dependencies ? (does apm do this already?)
+* respect package versions described in package.json
+* fallback to git if apm not installed?
+* skip installed packages on restore
+
+## ROADMAP
+
+* 0.1.0 - Initial Release
+
+Logo created by [David Chapman](https://thenounproject.com/david.chapman) from the [The Noun Project](thenounproject.com/)
---
+++ index.js
@@ -0,0 +1,252 @@
+#! /usr/bin/env node
+'use strict';
+
+var chalk        = require('chalk');
+var fs           = require('graceful-fs');
+var vfs          = require('vinyl-fs');
+var path         = require('path');
+var os           = require('os');
+var osenv        = require('osenv');
+var Q            = require('Q');
+var CSON         = require('season');
+var map          = require('vinyl-map');
+var util         = require('util');
+var stringify    = require('json-stringify-safe');
+var sh           = require('shelljs');
+var ProgressBar  = require('progress');
+
+var manifestName = 'manifest.json';
+var atomPath     = path.resolve(osenv.home(), '.atom');
+var argv         = require('minimist')(process.argv.slice(2));
+
+
+//====
+// check for apm
+//====
+if (!sh.which('apm')) {
+  process.stderr.write(chalk.bold.red('[Molecule Fatal Error]', 'APM is required to be installed to use Molecule.' + os.EOL));
+  process.exit(1);
+}
+//====
+//  Parse Command Line Arguments
+//====
+if (argv.b)
+{
+  getDisabledPackages().then(function(disabled) {
+    backup(disabled);
+  });
+}
+else if (argv.r)
+{
+  restore();
+}
+else if (argv.h || argv.help)
+{
+  help();
+}
+else
+{
+  help();
+}
+
+//====
+// CLI Commands
+//====
+function backup(disabled) {
+  checkAtomDirectory().then(function() {
+
+    process.stdout.write(chalk.bold.green('[Molecule] Backing up atom packages to', atomPath + '/' + manifestName + os.EOL));
+
+    var manifest = [];
+    var extractPkg = map(function(file) {
+
+      // file is a Buffer
+      file = file.toString();
+      var pkg;
+
+      try {
+        file = JSON.parse(file);
+      } catch (e) {
+        errors(e);
+        process.exit(1);
+      }
+
+      if (file.name && file.version) {
+        pkg = {
+          name: file.name,
+          version: file.version,
+          repo: (file.repository || null)
+        };
+      }
+      if (!disabled || (util.isArray(disabled) && disabled.indexOf(pkg.name) < 0)) {
+        manifest.push(pkg);
+      }
+    });
+
+    vfs.src('packages/*/package.json', {
+      cwd: atomPath
+    })
+    .pipe(extractPkg)
+    .on('end', function() {
+      try {
+        manifest = stringify(manifest, null, 2) + os.EOL;
+      } catch(e) {
+        errors(e);
+        process.exit(1);
+      }
+      fs.writeFile(path.join(atomPath, manifestName), manifest, function(err) {
+        if (err) {
+          errors(err);
+          process.exit(1);
+        }
+      });
+      process.stdout.write(chalk.bold.green('[Molecule] Backup Complete!' + os.EOL));
+    });
+
+
+  });
+}
+
+function restore() {
+  var pkgs;
+  checkAllAtomRequirements().then(function() {
+    process.stdout.write(chalk.bold.green('[Molecule] Restoring atom packages from', manifestName, 'at', atomPath + os.EOL));
+
+    fs.readFile(path.join(atomPath, manifestName), function(err, manifest) {
+      if (err) {
+        errors(err);
+      }
+
+      manifest = manifest.toString();
+
+      try {
+        manifest = JSON.parse(manifest);
+      } catch(e) {
+        errors(e);
+        process.exit(1);
+      }
+
+      if (!util.isArray(manifest)) {
+        errors(new Error('[Manifest Error] package restore expects manifest to be an array. exiting...'));
+        process.exit(1);
+      }
+
+      var bar = new ProgressBar(chalk.bold.yellow('  [:bar] :completed/:total'+ os.EOL), {
+        total: manifest.length,
+        callback: restoreComplete
+      });
+
+      function restoreComplete() {
+        process.stdout.write(chalk.bold.green('[Molecule] Restored ' + manifest.length + 'packages. Restore Complete!') + os.EOL);
+      }
+
+      process.stdout.write(os.EOL + chalk.bold.white('  ' + manifest.length + ' packages to install' + os.EOL + os.EOL));
+
+      var completed = 0;
+      manifest.forEach(function(pkg) {
+        // @TODO allow specification of package version
+        if (!pkg || !pkg.name) {
+          errors(new Error('[Molecule Error] package is malformed. skipping package.'));
+          return false;
+        }
+
+        sh.exec('apm install ' + pkg.name, {silent: argv.s}).output;
+        completed++;
+        bar.tick({
+          completed: completed,
+          total: manifest.length
+        });
+      });
+    });
+  });
+}
+
+function help() {
+  var eol = os.EOL;
+  process.stdout.write(
+    [
+      chalk.bold.green('Molecule'),
+      chalk.green('\tsimple CLI for dotfiles management of apm packages in Githubs\' Atom Editor'),
+      chalk.gray('\thttps://github.com/atom/atom'),
+      '[options]',
+      chalk.bold.green('\t-b') + chalk.green(', backup atom packages to ' + atomPath + '/' + manifestName),
+      chalk.bold.green('\t[-d]') + chalk.green(', backup disabled packages'),
+      eol,
+      chalk.bold.green('\t-r') + chalk.green(', restore atom packages from ' + atomPath + '/' + manifestName),
+      chalk.bold.green('\t[-s]') + chalk.green(', silent install output from apm'),
+      eol,
+      chalk.bold.green('\t-h') + chalk.green(', output help'),
+      chalk.bold.green('\t--help'),
+      eol
+    ].join(eol)
+  );
+  process.exit(0);
+}
+
+function getDisabledPackages() {
+  var deferred = Q.defer();
+  var disabled = null;
+
+  // forces the backup of disabled dependencies, defaults to skipping
+  if (!argv.d) {
+    CSON.readFile(path.join(atomPath, 'config.cson'), function(err, config) {
+      if (err) {
+        errors(err);
+      }
+
+      if (config && config['*'] && config['*'].core && config['*'].core.disabledPackages) {
+        disabled = config['*'].core.disabledPackages;
+        process.stdout.write(chalk.yellow('[Molecule] skipping disabled packages' + os.EOL + stringify(disabled, null, 2) + os.EOL));
+      }
+      deferred.resolve(disabled);
+    });
+  }
+  else {
+    deferred.resolve(disabled);
+  }
+
+  return deferred.promise;
+  }
+
+//======
+// Check Atom Editor Requirements
+//======
+
+function checkAllAtomRequirements() {
+  return checkAtomDirectory().then(function() {
+      return checkAtomPackagesManifest();
+    }
+  );
+}
+
+function checkAtomDirectory() {
+  var deferred = Q.defer();
+
+  fs.stat(atomPath, function(err, stats) {
+    if (err || !stats.isDirectory()) {
+      errors('.atom directory not found at ' + atomPath);
+      process.exit(1);
+    }
+    deferred.resolve();
+  });
+
+  return deferred.promise;
+}
+
+function checkAtomPackagesManifest() {
+  var deferred = Q.defer();
+
+  fs.stat(path.join(atomPath, manifestName), function(err, stats) {
+    if (err || !stats.isFile()) {
+      errors(manifestName, 'not found at', atomPath);
+      process.exit(1);
+    }
+    return deferred.resolve();
+  });
+
+  return deferred.promise;
+}
+
+function errors(err) {
+  process.stderr.write(chalk.bold.red('[Molecule Error]', err + os.EOL));
+}
