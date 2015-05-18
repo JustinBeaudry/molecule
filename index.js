56aae2e244b4e91ad1206632ca7aae304a2f0da6
JustinBeaudry <justin.beaudry+github@creativelive.com>
2015-05-18 00:31:23 -0700

    removing Q and doing all things sync

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ var vfs          = require('vinyl-fs');
 var path         = require('path');
 var os           = require('os');
 var osenv        = require('osenv');
-var Q            = require('Q');
 var CSON         = require('season');
 var map          = require('vinyl-map');
 var util         = require('util');
@@ -32,12 +31,13 @@ if (!sh.which('apm')) {
 //====
 if (argv.b)
 {
-  getDisabledPackages().then(function(disabled) {
-    backup(disabled);
-  });
+  checkAtomDirectory();
+  backup();
 }
 else if (argv.r)
 {
+  checkAtomDirectory();
+  checkAtomPackagesManifest();
   restore();
 }
 else if (argv.h || argv.help)
@@ -53,130 +53,127 @@ else
 // CLI Commands
 //====
 function backup(disabled) {
-  checkAtomDirectory().then(function() {
 
-    process.stdout.write(chalk.bold.green('[Molecule] Backing up atom packages to', atomPath + '/' + manifestName + os.EOL));
+  var disabled = null;
+  var config;
+  // forces the backup of disabled dependencies, defaults to skipping
+  if (!argv.d) {
+    config = CSON.readFileSync(path.join(atomPath, 'config.cson'));
 
-    var manifest = [];
-    var extractPkg = map(function(file, filename) {
+    if (config && config['*'] && config['*'].core && config['*'].core.disabledPackages) {
+      disabled = config['*'].core.disabledPackages;
+      process.stdout.write(chalk.yellow('[Molecule] skipping disabled packages' + os.EOL + stringify(disabled, null, 2) + os.EOL));
+    }
+  }
 
-      // file is a Buffer
-      file = file.toString();
-      var pkg;
+  process.stdout.write(chalk.bold.green('[Molecule] Backing up atom packages to', atomPath + '/' + manifestName + os.EOL));
 
-      try {
-        file = JSON.parse(file);
-      } catch (e) {
-        errors(e);
-        process.stderr.write(file + chalk.bold.red('failed JSON parsing at filename: ') + filename + os.EOL);
-        process.exit(1);
-      }
+  var manifest = [];
 
-      if (file.name && file.version) {
-        pkg = {
-          name: file.name,
-          version: file.version,
-          repo: (file.repository || null)
-        };
-      }
-      if (!disabled || (util.isArray(disabled) && disabled.indexOf(pkg.name) < 0)) {
-        manifest.push(pkg);
-      }
-    });
+  vfs.src('packages/*/package.json', {
+    cwd: atomPath
+  })
+  .pipe(map(function(file, filename) {
 
-    vfs.src('packages/*/package.json', {
-      cwd: atomPath
-    })
-    .pipe(extractPkg)
-    .on('end', function() {
-      try {
-        manifest = stringify(manifest, null, 2) + os.EOL;
-      } catch(e) {
-        errors(e);
-        process.exit(1);
-      }
-      fs.writeFile(path.join(atomPath, manifestName), manifest, function(err) {
-        if (err) {
-          errors(err);
-          process.exit(1);
-        }
-      });
-      process.stdout.write(chalk.bold.green('[Molecule] Backup Complete!' + os.EOL));
-    });
+    // file is a Buffer
+    file = file.toString();
+    var pkg;
 
+    try {
+      file = JSON.parse(file);
+    } catch (e) {
+      errors(e);
+      process.stderr.write(file + chalk.bold.red('failed JSON parsing at filename: ') + filename + os.EOL);
+      process.exit(1);
+    }
 
+    if (file.name && file.version) {
+      pkg = {
+        name: file.name,
+        version: file.version,
+        repo: (file.repository || null)
+      };
+    }
+    if (!disabled || (util.isArray(disabled) && disabled.indexOf(pkg.name) < 0)) {
+      manifest.push(pkg);
+    }
+  }))
+  .on('end', function() {
+    try {
+      manifest = stringify(manifest, null, 2) + os.EOL;
+    } catch(e) {
+      errors(e);
+      process.exit(1);
+    }
+    fs.writeFileSync(path.join(atomPath, manifestName), manifest);
+    process.stdout.write(chalk.bold.green('[Molecule] Backup Complete!' + os.EOL));
   });
+
 }
 
 function restore() {
   var pkgs;
-  checkAllAtomRequirements().then(function() {
 
-    var completed = 0;
-    var errored = 0;
-    var bar;
+  var completed = 0;
+  var errored = 0;
+  var bar;
 
-    process.stdout.write(chalk.bold.green('[Molecule] Restoring atom packages from', manifestName, 'at', atomPath + os.EOL));
+  process.stdout.write(chalk.bold.green('[Molecule] Restoring atom packages from', manifestName, 'at', atomPath + os.EOL));
 
-    fs.readFile(path.join(atomPath, manifestName), function(err, manifest) {
-      if (err) {
-        errors(err);
+  var manifest = fs.readFileSync(path.join(atomPath, manifestName));
+
+  manifest = manifest.toString();
+
+  try {
+    manifest = JSON.parse(manifest);
+  } catch(e) {
+    errors(e);
+    process.exit(1);
+  }
+
+  if (!util.isArray(manifest)) {
+    errors('package restore expects manifest to be an array. exiting...');
+    process.exit(1);
+  }
+
+  bar = new ProgressBar(
+    chalk.bold.yellow('  [:bar] :completed/:total') +
+    chalk.bold.red(' :errored error(s)')+
+    (argv.s ? '\r' : os.EOL),
+    {
+      total: manifest.length,
+      callback: function restoreComplete() {
+        var hasErrors = errored > 0;
+        var completeText = '[Molecule] Restore Complete!';
+        process.stdout.write(
+          chalk.bold.green('[Molecule] Restored ' + completed + '/' + manifest.length + ' packages. ') +
+          chalk.bold.red(errored + ' package') + (errored > 1 ? chalk.bold.red('s') : '') + chalk.bold.red(' failed.') + os.EOL +
+          (hasErrors ? chalk.bold.yellow(completeText) : chalk.bold.green(completeText)) + os.EOL
+        );
       }
+    }
+  );
 
-      manifest = manifest.toString();
+  process.stdout.write(os.EOL + chalk.bold.white('  ' + manifest.length + ' packages to install' + os.EOL + os.EOL));
 
-      try {
-        manifest = JSON.parse(manifest);
-      } catch(e) {
-        errors(e);
-        process.exit(1);
-      }
+  manifest.forEach(function(pkg) {
+    // @TODO allow specification of package version
+    if (!pkg || !pkg.name) {
+      errors('package is malformed. skipping package.');
+      return false;
+    }
 
-      if (!util.isArray(manifest)) {
-        errors('package restore expects manifest to be an array. exiting...');
-        process.exit(1);
-      }
+    if (sh.exec('apm install ' + pkg.name, {silent: argv.s}).code !== 0) {
+      errors('apm failed with a nonzero status code');
+      errored++;
+    } else {
+      completed++;
+    }
 
-      bar = new ProgressBar(
-        chalk.bold.yellow('  [:bar] :completed/:total') +
-        chalk.bold.red(' :errored error(s)')+
-        (argv.s ? '\r' : os.EOL),
-        {
-          total: manifest.length,
-          callback: function restoreComplete() {
-            var hasErrors = errored > 0;
-            var completeText = '[Molecule] Restore Complete!';
-            process.stdout.write(
-              chalk.bold.green('[Molecule] Restored ' + completed + '/' + manifest.length + ' packages. ') +
-              chalk.bold.red(errored + ' package') + (errored > 1 ? chalk.bold.red('s') : '') + chalk.bold.red(' failed.') + os.EOL +
-              (hasErrors ? chalk.bold.yellow(completeText) : chalk.bold.green(completeText)) + os.EOL
-            );
-          }
-        }
-      );
-
-      process.stdout.write(os.EOL + chalk.bold.white('  ' + manifest.length + ' packages to install' + os.EOL + os.EOL));
-
-      manifest.forEach(function(pkg) {
-        // @TODO allow specification of package version
-        if (!pkg || !pkg.name) {
-          errors('package is malformed. skipping package.');
-          return false;
-        }
-
-        if (sh.exec('apm install ' + pkg.name, {silent: argv.s}).code !== 0) {
-          errors('apm failed with a nonzero status code');
-          errored++;
-        } else {
-          completed++;
-        }
-
-        bar.tick({
-          errored: errored,
-          completed: completed,
-          total: manifest.length
-        });
-      });
+    bar.tick({
+      errored: errored,
+      completed: completed,
+      total: manifest.length
     });
   });
 }
@@ -203,68 +200,24 @@ function help() {
   process.exit(0);
 }
 
-function getDisabledPackages() {
-  var deferred = Q.defer();
-  var disabled = null;
-
-  // forces the backup of disabled dependencies, defaults to skipping
-  if (!argv.d) {
-    CSON.readFile(path.join(atomPath, 'config.cson'), function(err, config) {
-      if (err) {
-        errors(err);
-      }
-
-      if (config && config['*'] && config['*'].core && config['*'].core.disabledPackages) {
-        disabled = config['*'].core.disabledPackages;
-        process.stdout.write(chalk.yellow('[Molecule] skipping disabled packages' + os.EOL + stringify(disabled, null, 2) + os.EOL));
-      }
-      deferred.resolve(disabled);
-    });
-  }
-  else {
-    deferred.resolve(disabled);
-  }
-
-  return deferred.promise;
-  }
-
 //======
 // Check Atom Editor Requirements
 //======
 
-function checkAllAtomRequirements() {
-  return checkAtomDirectory().then(function() {
-      return checkAtomPackagesManifest();
-    }
-  );
-}
-
 function checkAtomDirectory() {
-  var deferred = Q.defer();
-
-  fs.stat(atomPath, function(err, stats) {
-    if (err || !stats.isDirectory()) {
-      errors('.atom directory not found at ' + atomPath);
-      process.exit(1);
-    }
-    deferred.resolve();
-  });
-
-  return deferred.promise;
+  var stats = fs.statSync(atomPath)
+  if (!stats.isDirectory()) {
+    errors('.atom directory not found at ' + atomPath);
+    process.exit(1);
+  }
 }
 
 function checkAtomPackagesManifest() {
-  var deferred = Q.defer();
-
-  fs.stat(path.join(atomPath, manifestName), function(err, stats) {
-    if (err || !stats.isFile()) {
-      errors(manifestName, 'not found at', atomPath);
-      process.exit(1);
-    }
-    return deferred.resolve();
-  });
-
-  return deferred.promise;
+  var stats = fs.statSync(path.join(atomPath, manifestName));
+  if (!stats.isFile()) {
+    errors(manifestName, 'not found at', atomPath);
+    process.exit(1);
+  }
 }
 
 function errors(err) {
